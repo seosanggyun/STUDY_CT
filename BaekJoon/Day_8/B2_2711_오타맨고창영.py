78f07172d06c5283ccfeb6cf06337a4a2ada36a0
seosanggyun <seosanggyun@naver.com>
2022-01-27 19:58:58 +0900

    0127 스터디 수정

--- BaekJoon/Day_8/B2_2711_오타맨고창영.py
+++ BaekJoon/Day_8/B2_2711_오타맨고창영.py
@@ -5,11 +5,23 @@ T = int(input())
 for TC in range(T):
 
     # 정보 입력 받음
-    Info = input().split()
-    answer = ''
-    # deleted = Info[1][Info[0]-1]
-    print(Info[1].replace(Info[1][int(Info[0])-1], '',1))
+    Info = input().split(' ')
+    # 오타를 낸 위치를 정수로 저장
+    order = int(Info[0])
 
-    ### 쒸익..
-    ### 이게 왜 문제가 있을까요 여러분
+    # 문자열을 리스트로 저장
+    # 왜 리스트로 저장하냐면
+    # 문자열로 해봤다가 피봤음
+    # 리스트로 하면 요소 변경이 쉬워
+    spells = list(Info[1])
+
+    # 스펠링 리스트에서 오타가 난 위치(order-1)의
+    # 스펠링을 제거함
+    del(spells[order-1])
+
+    # 리스트 형태의 spells를 문자열로 변환하여
+    # answer에 저장
+    answer = ''.join(spells)
+    print(answer)
+
     
